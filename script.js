a00cabc54dbca6e943492b5ca27cc4735fde29e9
aaman <aneela.aman.is@gmail.com>
2024-06-07 11:14:53 -0400

    create variables for the buttons and input

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// Project: Calculator
+
 let enterNumber= document.getElementById("enter");
+let clear= document.getElementById("clear");
+let deleteNumber = document.getElementById("delete");
+let percentNumber= document.getElementById("percent");
+let divideNumber= document.getElementById("divide");
+let multiplyNumber= document.getElementById("multiply");
+let addNumber= document.getElementById("add");
+let subtractNumber= document.getElementById("subtract");
+let numberOne= document.getElementById("1");
+let numberTwo= document.getElementById("2");
+let numberThree= document.getElementById("3");
+let numberFour= document.getElementById("4");
+let numberFive= document.getElementById("5");
+let numberSix= document.getElementById("6");
+let numberSeven= document.getElementById("7");
+let numberEight= document.getElementById("8");
+let numberNine= document.getElementById("9");
+let numberZero= document.getElementById("0");
+let numberDecimal= document.getElementById("decimal");
+let numberEqual= document.getElementById("equal");
+
+
+
