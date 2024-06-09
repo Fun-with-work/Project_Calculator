2351ff208bda7b9c43afd436947a74b2bfaebb9a
aaman <aneela.aman.is@gmail.com>
2024-06-08 20:07:30 -0400

    event listeners for numbers, variables for display

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// Project: Calculator
+        // Project: Calculator
 
-let enterNumber= document.getElementById("enter");
+let calcScreen= document.getElementById("calc");
+let previousNumber= document.getElementById("enter-previous");
+let currentNumber= document.getElementById("enter-current");
+let entryButtons= document.getElementById("entry-buttons");
 let clear= document.getElementById("clear");
 let deleteNumber = document.getElementById("delete");
 let percentNumber= document.getElementById("percent");
@@ -21,5 +24,100 @@ let numberZero= document.getElementById("0");
 let numberDecimal= document.getElementById("decimal");
 let numberEqual= document.getElementById("equal");
 
+let num1;
+let num2;
+let sum;
+
+clear.addEventListener("click", function() {
+    previousNumber.innerText ="";
+    currentNumber.innerText ="";
+
+});
+
+
+let calculator = function() {
+    numberOne.addEventListener("click", () => {
+        currentNumber.innerText += 1;
+    } );
+    
+    numberTwo.addEventListener("click", () => {
+        currentNumber.innerText += 2;
+    } );
+    
+    numberThree.addEventListener("click", () => {
+        currentNumber.innerText += 3;
+    } );
+    
+    numberFour.addEventListener("click", () => {
+        currentNumber.innerText += 4;
+    } );
+    
+    numberFive.addEventListener("click", () => {
+        currentNumber.innerText += 5;
+    } );
+
+    numberSix.addEventListener("click", () => {
+        currentNumber.innerText += 6;
+    } );
+
+    numberSeven.addEventListener("click", () => {
+        currentNumber.innerText += 7;
+    } );
+    
+    numberEight.addEventListener("click", () => {
+        currentNumber.innerText += 8;
+    } );
+    
+    numberNine.addEventListener("click", () => {
+        currentNumber.innerText += 9;
+    } );
+    
+    numberZero.addEventListener("click", () => {
+        currentNumber.innerText += 0;
+    } );
+    
+    numberDecimal.addEventListener("click", () => {
+        currentNumber.innerText += ".";
+    } );
+    
+    addNumber.addEventListener("click", () => {
+        currentNumber.innerText += "+";
+    } );
+    
+    subtractNumber.addEventListener("click", () => {
+        currentNumber.innerText += "-";
+    } );
+    
+    divideNumber.addEventListener("click", () => {
+        currentNumber.innerText += "/";
+    } );
+    
+    multiplyNumber.addEventListener("click", () => {
+        currentNumber.innerText += "*";
+    } );
+    
+    percentNumber.addEventListener("click", () => {
+        currentNumber.innerText += "%";
+    } );
+    
+    numberEqual.addEventListener("click", () => {
+        currentNumber.innerText += "=";
+    } );
+
+    
+};
+    
+calculator();
+
+
+// entryButton.addEventListener("click", function() {
+    
+
+    // console.log(currentNumber.value);
+//     console.log(numberTwo.value);
+//     console.log(percentNumber.value);
+      
+    
+// });
 
 
