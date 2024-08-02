d41f49b2dea798807f8795e8984c1164c0c75c7b
aaman <aneela.aman.is@gmail.com>
2024-08-02 13:01:14 -0400

    limit decimal to once. remove old code lines

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 let calcScreen= document.getElementById("calc-screen");
 let previousNumber= document.getElementById("previous-number");
-// let entryButtons= document.getElementById("entry-buttons");
 
 let input= document.getElementById("input");
 const numbers = document.querySelectorAll(".numbers")
@@ -11,22 +10,6 @@ let numberDecimal= document.getElementById("decimal");
 const clear= document.getElementById("clear");
 let deleteNumber = document.getElementById("delete");
 
-// let percentNumber= document.getElementById("percent");
-// let divideNumber= document.getElementById("divide");
-// let multiplyNumber= document.getElementById("multiply");
-// let addNumber= document.getElementById("add");
-// let subtractNumber= document.getElementById("subtract");
-// let numberOne= document.getElementById("1");
-// let numberTwo= document.getElementById("2");
-// let numberThree= document.getElementById("3");
-// let numberFour= document.getElementById("4");
-// let numberFive= document.getElementById("5");
-// let numberSix= document.getElementById("6");
-// let numberSeven= document.getElementById("7");
-// let numberEight= document.getElementById("8");
-// let numberNine= document.getElementById("9");
-// let numberZero= document.getElementById("0");
-// let numberEqual= document.getElementById("equal");
 
 let displayNumber = "0";
 
@@ -72,6 +55,27 @@ numbers.forEach(number => {
         }
     })
 });
+
+
+numberDecimal.addEventListener("click", e => {
+    if (operator ==="") { 
+        if (num1 - Math.floor(num1) ==0) {
+            num1 += e.target.innerText;
+        }    
+        else num1 = num1;
+    console.log (num1);
+  
+}
+    else {
+        if (num2 - Math.floor(num2) ==0) {
+            num2 += e.target.innerText;
+        }    
+        else num2 = num2;
+        console.log (num2);
+    
+    }
+
+})
 
 
 operators.forEach (op => {
@@ -138,95 +142,4 @@ operators.forEach (op => {
         }  
     });
 });
-
-
-// function Calculator(num1, num2, operator) {
-//     operators.forEach(operator => {
-//         operator.addEventListener("click", (e) => {
-//             num1 = displayNumber;
-//             console.log(num1);
-//             operator = e.target.innerText;
-//             console.log(operator);
-//         })
-//     })
-// }
-
-    // (input - Math.floor(input)) !== 0? 
-        // input.innerText += "."
-        // console.log ("decimal place")
-    // : input.innerText = input.innerText
-    // : console.log("it is whole")
-// } );    
-
-// addNumber.addEventListener("click", () => {
-//     num1 = Number (input.innerText);
-//     input.innerText = "";
-//     previousNumber.innerText += "+" + " " + num1 + " ";
-//     let sum = num1 + num2;
-//     totalNum = Number(sum.toFixed(4))
-//     num2 = num1;
-//     // input.innerText = (Number(totalNum.toFixed(4)));
-//     // num1 = 0;
-//     console.log (totalNum.toLocaleString());
-// } );
-    
-    
-// subtractNumber.addEventListener("click", () => {
-//     num1 = Number (input.innerText);
-//     input.innerText = "";
-//     previousNumber.innerText = num1 + " " + "-";
-//     let sum= num2 - num1;
-//     totalNum = Number(sum.toFixed(4))
-//     num2 = num1;
-//     input.innerText = (Number(totalNum.toFixed(4)));
-//     // num1 = 0;
-//     console.log (totalNum.toLocaleString());
-// } );
-
-// divideNumber.addEventListener("click", () => {
-//     num1 = Number (input.innerText);
-//     previousNumber.innerText += num1 + " " + "/";
-//     let sum = num2 / num1;
-//     totalNum = Number(sum.toFixed(4))
-//     num2 = num1;
-//     input.innerText = (Number(totalNum.toFixed(4)));
-//     // num1 = 0;
-//     console.log (totalNum.toLocaleString());
-// } );
-    
-// multiplyNumber.addEventListener("click", () => {
-//     num1 = Number (input.innerText);
-//     previousNumber.innerText += num1 + " " + "*";
-//     let sum = num2 * num1;
-//     totalNum = Number(sum.toFixed(4))
-//     num2 = num1;
-//     input.innerText = (Number(totalNum.toFixed(4)));
-//     // num1 = 0;
-//     console.log (totalNum.toLocaleString());
-// } );
-        
-// percentNumber.addEventListener("click", () => {
-//     num1 = Number (input.innerText);
-//     input.innerText = "";
-//     previousNumber.innerText = num1 + " " + "%";
-//     let sum = num2 % num1;
-//     totalNum = Number(sum.toFixed(4))
-//     num2 = num1;
-//     num1 = 0;
-//     console.log (totalNum.toLocaleString());
-
-
-// } );
-
-
-
-
-// entryButton.addEventListener("click", function() {
-
-    // console.log(input.value);
-//     console.log(numberTwo.value);
-//     console.log(percentNumber.value);
-      
-// });
-
 
